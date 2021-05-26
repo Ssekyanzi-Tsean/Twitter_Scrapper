3e7c4c9897856bbd9eaa5975f6e71154e67e3c3b
Shogun <msean481@gmail.com>
2021-05-26 07:42:57 +0300

    twitter handler composer

--- test/test_scrapy.py
+++ test/test_scrapy.py
@@ -6,4 +6,11 @@ def test_readCompanyName():
 
     company_name = scrapy.readCompanyName()
 
-    assert company_name == "unilever"
+    assert company_name == 'unilever'
+
+
+def test_compose_twitter_handler():
+    # Twitter Link Composer
+    company_name = 'glovo'
+    twitter_url = scrapy.compose_twitter_handler(company_name)
+    assert twitter_url == 'https://twitter.com/glovo?'
