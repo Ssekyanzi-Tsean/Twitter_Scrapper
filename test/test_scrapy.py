09e14596541d0225acd5f23ebabc1399c615ea92
Shogun <msean481@gmail.com>
2021-05-26 10:10:34 +0300

    3rd Test edits linker

--- test/test_scrapy.py
+++ test/test_scrapy.py
@@ -14,3 +14,10 @@ def test_compose_twitter_handler():
     company_name = 'glovo'
     twitter_url = scrapy.compose_twitter_handler(company_name)
     assert twitter_url == 'https://twitter.com/glovo?'
+
+
+def test_edit_link_to_contact():
+    # forms a contact link to the company website
+    company_name = 'cafejavas'
+    contact_link = scrapy.edit_link_to_contact(company_name)
+    assert contact_link == 'https://www.cafejavas.com/contact/'
